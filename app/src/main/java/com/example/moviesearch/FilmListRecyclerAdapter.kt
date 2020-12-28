88f764bccb6a0b6bc405b184f2cceb0450f572a9
Andrey <ankrnv@gmail.com>
2020-12-28 17:17:34 +0300

    5_task Edit RV:  - Create FilmViewHolder  - Create FilmListRecyclerAdapter

---
+++ app/src/main/java/com/example/moviesearch/FilmListRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.moviesearch
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+//в параметр передаем слушатель, чтобы мы потом могли обрабатывать нажатия из класса Activity
+class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    //Здесь у нас хранится список элементов для RV
+    private val items = mutableListOf<Film>()
+
+    //Этот метод нужно переопределить на возврат количества элементов в списке RV
+    override fun getItemCount() = items.size
+
+    //В этом методе мы привязываем наш ViewHolder и передаем туда "надутую" верстку нашего фильма
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return FilmViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.film_item, parent, false))
+    }
+
+    //В этом методе будет привязка полей из объекта Film к View из film_item.xml
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is FilmViewHolder -> {
+
+            }
+        }
+    }
+
+    //Метод для добавления объектов в наш список
+    fun addItems(list: List<Film>) {
+        //Сначала очищаем(если не реализовать DiffUtils)
+        items.clear()
+        //Добавляем
+        items.addAll(list)
+        //Уведомляем RV, что пришел новый список, и ему нужно заново все "привязывать"
+        notifyDataSetChanged()
+    }
+
+
+    //Интерфейс для обработки кликов
+    interface OnItemClickListener {
+        fun click(film: Film)
+    }
+}
---
+++ app/src/main/java/com/example/moviesearch/FilmViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.moviesearch
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.film_item.view.*
+
+//В конструктор класс передается layout, который мы создали(film_item.xml)
+class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    //Привязываем View из layout к переменным
+    private val title = itemView.title
+    private val poster = itemView.poster
+    private val description = itemView.description
+
+    //В этом методе кладем данные из Film в наши View
+    fun bind(film: Film) {
+        //Устанавливаем заголовок
+        title.text = film.title
+        //Устанавливаем постер
+        poster.setImageResource(film.poster)
+        //Устанавливаем описание
+        description.text = film.description
+    }
+}
