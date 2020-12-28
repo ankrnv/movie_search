9c1fb2442cd1e3f8808a848c9639038f4bd4444b
Andrey <ankrnv@gmail.com>
2020-12-28 17:04:20 +0300

    5_task Create class Film Add list of films

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -14,6 +14,20 @@ class MainActivity : AppCompatActivity() {
         initNavigation()
     }
 
+    val filmsDataBase = listOf(
+        Film("Star Wars", R.drawable.poster_1, "This should be a description"),
+        Film("Back to the Future", R.drawable.poster_2, "This should be a description"),
+        Film("PulpFiction", R.drawable.poster_3, "This should be a description"),
+        Film("Black Widow", R.drawable.poster_4, "This should be a description"),
+        Film("Jaws", R.drawable.poster_5, "This should be a description"),
+        Film("Blade Runner", R.drawable.poster_6, "This should be a description"),
+        Film("Spider-Man", R.drawable.poster_7, "This should be a description"),
+        Film("James Bond", R.drawable.poster_8, "This should be a description"),
+        Film("Attack", R.drawable.poster_9, "This should be a description"),
+        Film("Chicago", R.drawable.poster_10, "This should be a description"),
+        Film("Titanic", R.drawable.poster_11, "This should be a description")
+    )
+
     private fun initNavigation() {
         topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
