0076ba072fdf23c66074321b9a6ecca90a627f1f
Andrey <ankrnv@gmail.com>
2020-12-29 16:22:18 +0300

    5_task Add description

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -47,17 +47,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     val filmsDataBase = listOf(
-        Film("Star Wars", R.drawable.poster_1, "This should be a description"),
-        Film("Back to the Future", R.drawable.poster_2, "This should be a description"),
-        Film("PulpFiction", R.drawable.poster_3, "This should be a description"),
-        Film("Black Widow", R.drawable.poster_4, "This should be a description"),
-        Film("Jaws", R.drawable.poster_5, "This should be a description"),
-        Film("Blade Runner", R.drawable.poster_6, "This should be a description"),
-        Film("Spider-Man", R.drawable.poster_7, "This should be a description"),
-        Film("James Bond", R.drawable.poster_8, "This should be a description"),
-        Film("Attack", R.drawable.poster_9, "This should be a description"),
-        Film("Chicago", R.drawable.poster_10, "This should be a description"),
-        Film("Titanic", R.drawable.poster_11, "This should be a description")
+        Film("Star Wars", R.drawable.poster_1, "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader."),
+        Film("Back to the Future", R.drawable.poster_2, "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown."),
+        Film("Pulp Fiction", R.drawable.poster_3, "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."),
+        Film("Black Widow", R.drawable.poster_4, "A film about Natasha Romanoff in her quests between the films Civil War and Infinity War."),
+        Film("Jaws", R.drawable.poster_5, "When a killer shark unleashes chaos on a beach community, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down."),
+        Film("Blade Runner", R.drawable.poster_6, "A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator."),
+        Film("Spider-Man", R.drawable.poster_7, "Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities."),
+        Film("James Bond", R.drawable.poster_8, "James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology."),
+        Film("Attack", R.drawable.poster_9, "When an abused socialite grows to giant size because of an alien encounter and an aborted murder attempt, she goes after her cheating husband with revenge on her mind."),
+        Film("Chicago", R.drawable.poster_10, "Two death-row murderesses develop a fierce rivalry while competing for publicity, celebrity, and a sleazy lawyer's attention."),
+        Film("Titanic", R.drawable.poster_11, "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.")
     )
 
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
