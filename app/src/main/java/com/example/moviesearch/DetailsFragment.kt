9d6326915e38376a2e3c3c9a274d0d2d8ab73678
Andrey <ankrnv@gmail.com>
2021-01-14 15:33:49 +0300

    6_task Finished 6_Task

--- app/src/main/java/com/example/moviesearch/DetailsFragment.kt
+++ app/src/main/java/com/example/moviesearch/DetailsFragment.kt
@@ -5,13 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_details.*
 
-class DetailsFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_details)
+class DetailsFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
         //Получаем наш фильм из переданного бандла
         val film = arguments?.get("film") as Film
 
@@ -22,12 +34,4 @@ class DetailsFragment : AppCompatActivity() {
         //Устанавливаем описание
         details_description.text = film.description
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
-    }
-
 }
