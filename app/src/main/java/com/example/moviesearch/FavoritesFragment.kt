500279ca22b372128371effe3c1ff50247f8fecd
Andrey <ankrnv@gmail.com>
2021-01-20 15:48:37 +0300

    7_Task Create fragment_favorites.xml

---
+++ app/src/main/java/com/example/moviesearch/FavoritesFragment.kt
@@ -0,0 +1,36 @@
+package com.example.moviesearch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_favorites.*
+
+class FavoritesFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Получаем список при транзакции фрагмента
+        val favoritesList: List<Film> = emptyList()
+
+        favorites_recycler
+            .apply {
+                filmsAdapter =
+                    FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
+                        override fun click(film: Film) {
+                            (requireActivity() as MainActivity).launchDetailsFragment(film)
+                        }
+                    })
+                //Присваиваем адаптер
+                adapter = filmsAdapter
+                //Присвои layoutmanager
+                layoutManager = LinearLayoutManager(requireContext())
+                //Применяем декоратор для отступов
+                val decorator = TopSpacingItemDecoration(8)
+                addItemDecoration(decorator)
+            }
+        //Кладем нашу БД в RV
+        filmsAdapter.add
+    }
+}
