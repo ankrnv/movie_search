d4b3c254d38b3e40682da94cdfbb0f6478a404fe
Andrey <ankrnv@gmail.com>
2021-02-16 17:14:20 +0300

    11_Task Add first test RV created

---
+++ app/src/androidTest/java/com/example/moviesearch/MainActivityTest.kt
@@ -0,0 +1,30 @@
+package com.example.moviesearch
+
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun recyclerViewShouldBeAttached() {
+        onView(withId(R.id.main_recycler)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<FilmViewHolder>(0, click()))
+    }
+}
