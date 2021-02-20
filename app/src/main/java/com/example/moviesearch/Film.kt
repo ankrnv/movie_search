559129b380386d7491018113a5e0245f56c83754
Andrey <ankrnv@gmail.com>
2021-02-20 15:41:37 +0300

    12_Task Finished

--- app/src/main/java/com/example/moviesearch/Film.kt
+++ app/src/main/java/com/example/moviesearch/Film.kt
@@ -9,5 +9,6 @@ data class Film(
     val title: String,
     val poster: Int,
     val description: String,
+    var rating: Float = 0f,
     var isInFavorites: Boolean = false
 ) : Parcelable
--- app/src/main/java/com/example/moviesearch/FilmViewHolder.kt
+++ app/src/main/java/com/example/moviesearch/FilmViewHolder.kt
@@ -6,16 +6,19 @@ import kotlinx.android.synthetic.main.film_item.view.*
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    //Привязываем View из layout к переменным
+    //Привязываем view из layout к переменным
     private val title = itemView.title
     private val poster = itemView.poster
     private val description = itemView.description
+    //Вот здесь мы находим в верстке наш прогресс бар для рейтинга
+    private val ratingDonut = itemView.rating_donut
 
-    //В этом методе кладем данные из Film в наши View
+    //В этом методе кладем данные из film в наши view
     fun bind(film: Film) {
         //Устанавливаем заголовок
         title.text = film.title
-        //Указываем контейнер, в котором будет "жить" наша картинка
+        //Устанавливаем постер
+        //Указываем контейнер, в которм будет "жить" наша картинка
         Glide.with(itemView)
             //Загружаем сам ресурс
             .load(film.poster)
@@ -25,5 +28,7 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .into(poster)
         //Устанавливаем описание
         description.text = film.description
+        //Устанавливаем рэйтинг
+        ratingDonut.setProgress((film.rating * 10).toInt())
     }
 }
