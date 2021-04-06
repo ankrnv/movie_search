b3b42f23f91f3c46b4d5664a7e01f1a5a6b6db65
Andrey <ankrnv@gmail.com>
2021-04-06 17:03:09 +0300

    Add instance Dagger in App

--- app/src/main/java/com/example/moviesearch/App.kt
+++ app/src/main/java/com/example/moviesearch/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.moviesearch.data.ApiConstants
 import com.example.moviesearch.data.MainRepository
 import com.example.moviesearch.data.TmdbApi
+import com.example.moviesearch.di.AppComponent
 import com.example.moviesearch.di.DI
 import com.example.moviesearch.domain.Interactor
 import okhttp3.OkHttpClient
@@ -16,16 +17,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class App : Application() {
+    lateinit var dagger: AppComponent
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            //Прикрепляем контекст
-            androidContext(this@App)
-            //(Опционально) подключаем зависимость
-            androidLogger()
-            //Инициализируем модули
-            modules(listOf(DI.mainModule))
-        }
+        instance = this
+        //Создаем компонент
+        dagger = DaggerAppComponent.create()
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
     }
 }
