968494fa129a4b853cc6d15b5ec462e3c5b4d946
Andrey <ankrnv@gmail.com>
2021-04-21 15:36:15 +0300

    36 Task Create PreferenceProvaider.kt edit TmdbApi.kt

--- app/src/main/java/com/example/moviesearch/data/TmdbApi.kt
+++ app/src/main/java/com/example/moviesearch/data/TmdbApi.kt
@@ -3,11 +3,13 @@ package com.example.moviesearch.data
 import com.example.moviesearch.data.Entity.TmdbResults
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApi {
-    @GET("3/movie/popular")
+    @GET("3/movie/{category}")
     fun getFilms(
+        @Path("category") category: String,
         @Query("api_key") apiKey: String,
         @Query("language") language: String,
         @Query("page") page: Int
