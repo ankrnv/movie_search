959b26d7e16d3f1a7556f887bc3955400207c23c
Andrey <ankrnv@gmail.com>
2021-04-27 16:39:57 +0300

    37 Task Edit DatabaseHelper.kt

--- app/src/main/java/com/example/moviesearch/data/Entity/db/DatabaseHelper.kt
+++ app/src/main/java/com/example/moviesearch/data/Entity/db/DatabaseHelper.kt
@@ -1,4 +1,35 @@
 package com.example.moviesearch.data.Entity.db
 
-class DatabaseHelper {
+import android.app.DownloadManager.*
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null,  DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "$COLUMN_TITLE TEXT UNIQUE," +
+                    "$COLUMN_POSTER TEXT," +
+                    "$COLUMN_DESCRIPTION TEXT," +
+                    "$COLUMN_RATING REAL);"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private  const val DATABASE_NAME = "films.db"
+        private  const val DATABASE_VERSION = 1
+
+
+        const val TABLE_NAME = "films_table"
+        const val COLUMN_ID = "id"
+        const val COLUMN_TITLE = "title"
+        const val COLUMN_POSTER = "poster_path"
+        const val COLUMN_DESCRIPTION = "overview"
+        const val COLUMN_RATING = "vote_average"
+    }
 }
