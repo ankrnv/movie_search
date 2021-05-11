8f5e048f5046e1880571cf762e441628f2ab60b3
Andrey <ankrnv@gmail.com>
2021-05-11 16:12:48 +0300

    38 Task Edit main repository

--- app/src/main/java/com/example/moviesearch/data/MainRepository.kt
+++ app/src/main/java/com/example/moviesearch/data/MainRepository.kt
@@ -4,54 +4,67 @@ import android.content.ContentValues
 import android.database.Cursor
 import com.example.moviesearch.data.Entity.db.AppDatabase
 import com.example.moviesearch.data.Entity.Film
+import com.example.moviesearch.data.Entity.dao.FilmDao
+import java.util.concurrent.Executors
 
-class MainRepository(databaseHelper: AppDatabase) {
-    //Инициализируем объект для взаимодействия с БД
-    private val sqlDb = databaseHelper.readableDatabase
-    //Создаем курсор для обработки запросов из БД
-    private lateinit var cursor: Cursor
+class MainRepository(private val filmDao: FilmDao) {
 
-    fun putToDb(film: Film) {
-        //Создаем объект, который будет хранить пары ключ-значение, для того
-        //чтобы класть нужные данные в нужные столбцы
-        val cv = ContentValues()
-        cv.apply {
-            put(AppDatabase.COLUMN_TITLE, film.title)
-            put(AppDatabase.COLUMN_POSTER, film.poster)
-            put(AppDatabase.COLUMN_DESCRIPTION, film.description)
-            put(AppDatabase.COLUMN_RATING, film.rating)
+    fun putToDb(films: List<Film>) {
+        Executors.newSingleThreadExecutor().execute {
+            filmDao.insertAll(films)
         }
-        //Кладем фильм в БД
-        sqlDb.insert(AppDatabase.TABLE_NAME, null, cv)
     }
 
     fun getAllFromDB(): List<Film> {
-        //Создаем курсор на основании запроса "Получить все из таблицы"
-        cursor = sqlDb.rawQuery("SELECT * FROM ${AppDatabase.TABLE_NAME}", null)
-        //Сюда будем сохранять результат получения данных
-        val result = mutableListOf<Film>()
-        //Проверяем, есть ли хоть одна строка в ответе на запрос
-        if (cursor.moveToFirst()) {
-            //Итерируемся по таблице, пока есть записи, и создаем на основании объект Film
-            do {
-                val title = cursor.getString(1)
-                val poster = cursor.getString(2)
-                val description = cursor.getString(3)
-                val rating = cursor.getDouble(4)
-
-                result.add(
-                    Film(
-                        title,
-                        poster,
-                        description,
-                        rating
-                    )
-                )
-            } while (cursor.moveToNext())
-        }
-        //Возвращаем список фильмов
-        return result
+        return filmDao.getCachedFilms()
     }
+
+//    //Инициализируем объект для взаимодействия с БД
+//    private val sqlDb = databaseHelper.readableDatabase
+//    //Создаем курсор для обработки запросов из БД
+//    private lateinit var cursor: Cursor
+//
+//    fun putToDb(film: Film) {
+//        //Создаем объект, который будет хранить пары ключ-значение, для того
+//        //чтобы класть нужные данные в нужные столбцы
+//        val cv = ContentValues()
+//        cv.apply {
+//            put(AppDatabase.COLUMN_TITLE, film.title)
+//            put(AppDatabase.COLUMN_POSTER, film.poster)
+//            put(AppDatabase.COLUMN_DESCRIPTION, film.description)
+//            put(AppDatabase.COLUMN_RATING, film.rating)
+//        }
+//        //Кладем фильм в БД
+//        sqlDb.insert(AppDatabase.TABLE_NAME, null, cv)
+//    }
+//
+//    fun getAllFromDB(): List<Film> {
+//        //Создаем курсор на основании запроса "Получить все из таблицы"
+//        cursor = sqlDb.rawQuery("SELECT * FROM ${AppDatabase.TABLE_NAME}", null)
+//        //Сюда будем сохранять результат получения данных
+//        val result = mutableListOf<Film>()
+//        //Проверяем, есть ли хоть одна строка в ответе на запрос
+//        if (cursor.moveToFirst()) {
+//            //Итерируемся по таблице, пока есть записи, и создаем на основании объект Film
+//            do {
+//                val title = cursor.getString(1)
+//                val poster = cursor.getString(2)
+//                val description = cursor.getString(3)
+//                val rating = cursor.getDouble(4)
+//
+//                result.add(
+//                    Film(
+//                        title,
+//                        poster,
+//                        description,
+//                        rating
+//                    )
+//                )
+//            } while (cursor.moveToNext())
+//        }
+//        //Возвращаем список фильмов
+//        return result
+//    }
 }
 
 //    val filmsDataBase = listOf(
