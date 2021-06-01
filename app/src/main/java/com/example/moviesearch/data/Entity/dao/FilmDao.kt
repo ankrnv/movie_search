86e3e0cb0070f7a97bbe4b4ebe20da78859ce0d5
andrey <ankrnv@gmail.com>
2021-06-02 00:17:00 +0300

    Task_41 Edit DAO

--- app/src/main/java/com/example/moviesearch/data/Entity/dao/FilmDao.kt
+++ app/src/main/java/com/example/moviesearch/data/Entity/dao/FilmDao.kt
@@ -6,11 +6,12 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.moviesearch.data.Entity.Film
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FilmDao {
     @Query("SELECT * FROM cached_films")
-    fun getCachedFilms(): LiveData<List<Film>>
+    fun getCachedFilms(): Flow<List<Film>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(list: List<Film>)
