4aa4a1383ad778d4843d7fe454ad5acc8bb4cb16
andrey <ankrnv@gmail.com>
2021-06-08 21:36:05 +0300

    Task_42 Edit Gradle

--- app/src/main/java/com/example/moviesearch/view/rv_viewholders/FilmViewHolder.kt
+++ app/src/main/java/com/example/moviesearch/view/rv_viewholders/FilmViewHolder.kt
@@ -1,21 +1,21 @@
 package com.example.moviesearch.view.rv_viewholders
+
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.moviesearch.data.ApiConstants
 import com.example.moviesearch.data.Entity.Film
 import com.example.moviesearch.databinding.FilmItemBinding
-import kotlinx.android.synthetic.main.film_item.view.*
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    //Привязываем view из layout к переменным
-    private val title = FilmItemBinding.title
-    private val poster = FilmItemBinding.poster
-    private val description = FilmItemBinding.description
-    //Вот здесь мы находим в верстке наш прогресс бар для рейтинга
-    private val ratingDonut = FilmItemBinding.rating_donut
     private val filmItemBinding = FilmItemBinding.bind(itemView)
+    //Привязываем view из layout к переменным
+    private val title = filmItemBinding.title
+    private val poster = filmItemBinding.poster
+    private val description = filmItemBinding.description
+    //Вот здесь мы находим в верстке наш прогресс бар для рейтинга
+    private val ratingDonut = filmItemBinding.ratingDonut
 
     //В этом методе кладем данные из film в наши view
     fun bind(film: Film) {
