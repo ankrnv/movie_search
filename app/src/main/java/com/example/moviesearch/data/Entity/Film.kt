0a148e13aed8e0b01185b5fe40fbef2b1ef7fe57
andrey <ankrnv@gmail.com>
2021-06-22 19:31:50 +0300

    Task_43 Edit Interactor.kt

--- app/src/main/java/com/example/moviesearch/data/Entity/Film.kt
+++ app/src/main/java/com/example/moviesearch/data/Entity/Film.kt
@@ -12,7 +12,7 @@ import kotlinx.parcelize.Parcelize
 data class Film(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "poster_path") val poster: String,
+    @ColumnInfo(name = "poster_path") val poster: String?,
     @ColumnInfo(name = "overview") val description: String,
     @ColumnInfo(name = "vote_average") val rating: Double = 0.0,
     var isInFavorites: Boolean = false
