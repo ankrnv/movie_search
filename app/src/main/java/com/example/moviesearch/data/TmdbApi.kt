a8361fa19a446d9f81e76a107fc2e1c7a8c2cdbf
andrey <ankrnv@gmail.com>
2021-06-22 13:15:42 +0300

    Task_43 Starting 43 Task

--- app/src/main/java/com/example/moviesearch/data/TmdbApi.kt
+++ app/src/main/java/com/example/moviesearch/data/TmdbApi.kt
@@ -1,6 +1,7 @@
 package com.example.moviesearch.data
 
 import com.example.moviesearch.data.Entity.TmdbResults
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,4 +15,12 @@ interface TmdbApi {
         @Query("language") language: String,
         @Query("page") page: Int
     ): Call<TmdbResults>
+
+    @GET("3/search/movie")
+    fun getFilmFromSearch(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("query") query: String,
+        @Query("page") page: Int
+    ): Observable<TmdbResults>
 }
