654ae8b402dbcb409e6279a865c2cf0d5c349c31
andrey <ankrnv@gmail.com>
2021-06-25 16:19:12 +0300

    Task_44 Refactoring

--- app/src/main/java/com/example/moviesearch/data/ApiConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviesearch.data
-
-object ApiConstants {
-    const val BASE_URL = "https://api.themoviedb.org/"
-    const val IMAGES_URL = "https://image.tmdb.org/t/p/"
-}
--- remote_module/src/main/java/com/example/remote_module/TmdbApi.kt
+++ remote_module/src/main/java/com/example/remote_module/TmdbApi.kt
@@ -1,4 +1,26 @@
 package com.example.remote_module
 
+import com.example.remote_module.entity.TmdbResults
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface TmdbApi {
+    @GET("3/movie/{category}")
+    fun getFilms(
+        @Path("category") category: String,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("page") page: Int
+    ): Call<TmdbResults>
+
+    @GET("3/search/movie")
+    fun getFilmFromSearch(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("query") query: String,
+        @Query("page") page: Int
+    ): Observable<TmdbResults>
 }
