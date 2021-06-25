8e37861462a8dcb4c6e1155fad22299ae34b788a
andrey <ankrnv@gmail.com>
2021-06-25 16:16:04 +0300

    Task_44 Edit entity

--- remote_module/src/main/java/com/example/remote_module/entity/ApiConstants.kt
+++ remote_module/src/main/java/com/example/remote_module/entity/ApiConstants.kt
@@ -1,4 +1,6 @@
 package com.example.remote_module.entity
 
 object ApiConstants {
+    const val BASE_URL = "https://api.themoviedb.org/"
+    const val IMAGES_URL = "https://image.tmdb.org/t/p/"
 }
--- remote_module/src/main/java/com/example/remote_module/entity/TmdbResults.kt
+++ remote_module/src/main/java/com/example/remote_module/entity/TmdbResults.kt
@@ -1,4 +1,14 @@
 package com.example.remote_module.entity
 
-class TmdbResults {
-}
+import com.google.gson.annotations.SerializedName
+
+data class TmdbResults(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val tmdbFilms: List<TmdbFilm>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
