50404157139415a15995d2f1e1358600e9f93913
Andrey <ankrnv@gmail.com>
2021-07-02 16:28:11 +0300

    45 Task Finished 45 Task

--- app/src/main/java/com/example/moviesearch/view/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.moviesearch.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,11 +10,13 @@ import com.example.moviesearch.view.fragments.HomeFragment
 import com.example.moviesearch.R
 import com.example.moviesearch.databinding.ActivityMainBinding
 import com.example.moviesearch.data.Entity.Film
+import com.example.moviesearch.receivers.ConnectionChecker
 import com.example.moviesearch.view.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +35,17 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
 
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
     fun launchDetailsFragment(film: Film) {
