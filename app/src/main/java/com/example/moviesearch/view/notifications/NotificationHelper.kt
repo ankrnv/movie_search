c3b812057559a23776f5fde29d62e38bd6653514
Andrey <ankrnv@gmail.com>
2021-07-12 15:55:47 +0300

    46 Task Add notifications

--- app/src/main/java/com/example/moviesearch/view/notifications/NotificationHelper.kt
+++ app/src/main/java/com/example/moviesearch/view/notifications/NotificationHelper.kt
@@ -3,11 +3,17 @@ package com.example.moviesearch.view.notifications
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.moviesearch.R
 import com.example.moviesearch.data.Entity.Film
 import com.example.moviesearch.view.MainActivity
+import com.example.remote_module.entity.ApiConstants
 
 object NotificationHelper {
     fun createNotification(context: Context, film: Film) {
@@ -25,6 +31,25 @@ object NotificationHelper {
         }
 
         val notificationManager = NotificationManagerCompat.from(context)
+
+        Glide.with(context)
+            //говорим что нужен битмап
+            .asBitmap()
+            //указываем откуда загружать, это ссылка как на загрузку с API
+            .load(ApiConstants.IMAGES_URL + "w500" + film.poster)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+                //Этот коллбэк отрабатоет когда мы успешно получим битмап
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    //Создаем нотификации в стиле big picture
+                    builder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(resource))
+                    //Обновляем нотификацю
+                    notificationManager.notify(film.id, builder.build())
+                }
+            })
+        //Отправляем изначальную нотификацю в стандартном исполнении
+        notificationManager.notify(film.id, builder.build())
 
     }
 }
